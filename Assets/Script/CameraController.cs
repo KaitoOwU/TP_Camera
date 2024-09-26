3b8065651dbb9443eb34659d543d55f42ce933a1
Kévin <advestankv@gmail.com>
2024-09-26 10:29:39 +0200

    [ADD] aujourd'hui maman est morte

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -8,7 +8,9 @@ public class CameraController : MonoBehaviour
 {
     public static CameraController Instance { get; private set; }   
     public Camera MainCamera { get; private set; }
-    public CameraConfiguration Configuration { get; private set; }
+    private CameraConfiguration _configuration;
+    
+    private List<AView> _activeViews = new List<AView>();
 
     private void Awake()
     {
@@ -27,8 +29,54 @@ public class CameraController : MonoBehaviour
 
     private void ApplyConfiguration()
     {
-        MainCamera.transform.rotation = Configuration.GetRotation();
-        MainCamera.transform.position = Configuration.GetPosition();
+        MainCamera.transform.rotation = _configuration.GetRotation();
+        MainCamera.transform.position = _configuration.GetPosition();
     }
+
+    public void AddView(AView view)
+    {
+        _activeViews.Add(view);
+    }
+
+    public void RemoveView(AView view)
+    {
+        _activeViews.Remove(view);
+    }
+
+    public CameraConfiguration ComputeAverage()
+    {
+        CameraConfiguration newConfig = new();
+        float weight = 0f;
+
+        foreach (AView view in _activeViews)
+        {
+            weight += view.weight;
+            newConfig.pitch += view.GetConfiguration().pitch * view.weight;
+            newConfig.roll += view.GetConfiguration().roll * view.weight;
+            newConfig.distance += view.GetConfiguration().distance * view.weight;
+            newConfig.fov += view.GetConfiguration().fov * view.weight;
+            newConfig.pivot += view.GetConfiguration().pivot * view.weight;
+        }
+
+        newConfig.yaw = ComputeAverageYaw();
+        newConfig.pitch /= weight;
+        newConfig.roll /= weight;
+        newConfig.distance /= weight;
+        newConfig.fov /= weight;
+        newConfig.pivot /= weight;
+    }
+    
+    private float ComputeAverageYaw()
+    {
+        Vector2 sum = Vector2.zero;
+        foreach (AView view in _activeViews)
+        {
+            CameraConfiguration config = view.GetConfiguration();
+            sum += new Vector2(Mathf.Cos(config.yaw * Mathf.Deg2Rad),
+                Mathf.Sin(config.yaw * Mathf.Deg2Rad)) * view.weight;
+        }
+        return Vector2.SignedAngle(Vector2.right, sum);
+    }
+    
     
 }
